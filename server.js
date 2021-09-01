0e0be90f5faf8fc3b93a11853ddbbd1c626b44ea
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-09-01 15:02:28 -0500

    adding app.get/ notes html

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.port || 3001;
+
+app.use(express.static('public'));
+
+// Getting notes page from assets.  
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/assets/notes.html'))
+);
+
+// Need to use in order to hook into our website. otherwise it wont load.
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
