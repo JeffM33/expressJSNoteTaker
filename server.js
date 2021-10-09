a5471328c8b703af8effee49f4994f2bbc768b08
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-10-09 17:20:05 -0500

    fixed server errors with code added express added index.html, and notes.html

--- server.js
+++ server.js
@@ -1,26 +1,19 @@
 const express = require('express');
 const path = require('path');
 
+const app = express();
 const PORT = process.env.port || 3001;
 
-const app = express();
-
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-)
+app.use(express.static('public'));
 
 // Getting notes page from assets.  
 app.get('/notes', (req, res) => 
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-//Setting 404 error
-app.get('*', (req, res) => 
-  res.sendFile(path.join(__dirname, 'public/404.html'))
+// getting index.html
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 // Need to use in order to hook into our website. otherwise it wont load.
