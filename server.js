5663c892387ce7a50fcaed051d178a1e206ff7c1
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-10-11 10:12:18 -0500

    Need to fix post unable to read db file getting error.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
-const notes = require('./db/db.json');
+const fs = require('fs');
 
+const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
 const app = express();
 const PORT = process.env.port || 3001;
 
@@ -19,12 +20,30 @@ app.get('*', (req, res) =>{
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-// app.get('/api/notes', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/db/db.json'));
+// Gets all notes
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './db/db.json'));
+});
 
-//   req.method(notes);
-// });
+// Getting notes by id
+app.get('/api/notes/:id', (req, res) => {
+  res.json(notes[Number(req.params.id)]);
+});
 
+// Creating the notes
+app.post('./api/notes', (req, res) => {
+  let note = req.body;
+  let id = (notes.length).toString();
+
+  note.id = id;
+  notes.push(note);
+
+  console.log(id);
+  console.log(note);
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+  res.json(notes);
+});
 
 // Need to use in order to hook into our website. otherwise it wont load.
 app.listen(PORT, () =>{
