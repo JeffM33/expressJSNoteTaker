b34ca09c1ec171133e3d77e7b30399e9eb3de302
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-10-11 16:46:30 -0500

    finished working project

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const {
+  readFromFile,
+  writeToFile,
+} = require('./helpers/fsUtils');
+
 
 const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
 const app = express();
@@ -13,6 +18,7 @@ app.use(express.urlencoded({ extended: true}));
 // Getting notes page from assets.  
 app.get('/notes', (req, res) =>{ 
   res.sendFile(path.join(__dirname, '/public/notes.html'));
+
 });
 
 // getting index.html
@@ -23,6 +29,7 @@ app.get('/', (req, res) =>{
 // Gets all notes
 app.get('/api/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/db/db.json'));
+  //readFromFile('/db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // Getting notes by id
@@ -43,6 +50,27 @@ app.post('/api/notes', (req, res) => {
 
   fs.writeFileSync('./db/db.json', JSON.stringify(notes));
   res.json(notes);
+});
+
+// Deleting a specific note
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  // console.log(noteId, 'logging note id after req.params');
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all db except the one with the ID provided in the URL
+      // console.log(noteId, 'logging note id after the .then statements');
+      const result = json.filter((notes) => notes.id !== noteId);
+      // console.log(notes, 'Logging notes after result');
+      // console.log(result, 'logging result after filtering json');
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+      
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+      // console.log(noteId, 'logging note id once the delete request has been declared.');
+    });
 });
 
 // Need to use in order to hook into our website. otherwise it wont load.
